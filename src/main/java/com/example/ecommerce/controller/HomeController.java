ed7866d70c35ffa771077c76972ca5a2363eec35
olek2303 <olek2303@gmail.com>
2024-02-11 22:29:04 +0100

    initial commit

---
+++ src/main/java/com/example/ecommerce/controller/HomeController.java
@@ -0,0 +1,14 @@
+package com.example.ecommerce.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class HomeController {
+
+    @RequestMapping(value = {"", "/", "/home"}) // dostep do strony glownej z /, , /home
+    public String displayHomePage() {
+        return "home.html";
+    }
+
+}
