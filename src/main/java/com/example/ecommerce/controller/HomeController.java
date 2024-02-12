4034284d6b91f2dec3bddf10867c40db466c6c45
olek2303 <olek2303@gmail.com>
2024-02-12 16:45:32 +0100

    added html, js, and css files and request mapping in spring

--- src/main/java/com/example/ecommerce/controller/HomeController.java
+++ src/main/java/com/example/ecommerce/controller/HomeController.java
@@ -1,5 +1,6 @@
 package com.example.ecommerce.controller;
 
+import org.springframework.ui.Model;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,8 +8,12 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
 
     @RequestMapping(value = {"", "/", "/home"}) // dostep do strony glownej z /, , /home
-    public String displayHomePage() {
+    public String displayHomePage(Model model) {
+
+        model.addAttribute("username", "Aleksander");
         return "home.html";
     }
+
+
 
 }
